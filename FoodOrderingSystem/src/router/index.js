0999ec40c75fc7d8273d9a04982d5636c28dc2e8
0elek255w <187811097+0elek255w@users.noreply.github.com>
2025-03-07 00:46:42 +0300

    база для проекта

---
+++ FoodOrderingSystem/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Home.vue'
+import Cart from '../views/Cart.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/cart',  
+      name: 'cart',
+      component: Cart,
+    },
+  ],
+})
+
+export default router
